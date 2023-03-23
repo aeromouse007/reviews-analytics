df47d6da96a8c6d61a18b3dd104b39f5d24bf5df
aeromouse007 <94617584+aeromouse007@users.noreply.github.com>
2023-03-23 19:23:55 +0800

    add filter

--- reading.py
+++ reading.py
@@ -7,3 +7,16 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print('共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data: 
+	sum_len = sum_len + len(d)
+print('平均長度為',sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
